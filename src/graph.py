1c28ab31dfac70674cf0c076ae57da8e0978f07d
TimWeichsel <weichselberger.tim@icloud.com>
2026-05-23 19:53:09 +0200

    updated agent flow

--- src/graph.py
+++ src/graph.py
@@ -3,13 +3,16 @@ temperature=0.3
 #Imports
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
-from langgraph.graph import StateGraph, START, MessagesState
+from langgraph.graph import StateGraph, START, END, MessagesState
 from langchain_core.messages import SystemMessage, HumanMessage
 from langgraph.prebuilt import tools_condition, ToolNode
 from typing import Optional, Literal, TypedDict
 
 from src.services.knowledge_retriever import load_paper_information
 from src.services.arxiv_client import arxiv_paper_search
+
+from utils.file_utils import load_file, save_file, append_to_file
+
 
 # Load all environment variables from .env file
 load_dotenv()
@@ -28,37 +31,50 @@ sys_msg = SystemMessage('''You are a machine learning research expert.
     Focus on papers that build on existing knowledge without repeating it
     Return the following an The Title and the Explanation of the paper''')
 
+paper_titel_msg = SystemMessage('''You are a machine learning research expert.
+    Your goal is to extract the title of the given paper information. Don't return anyhing but just the title.''')
+
+
+paper_base_update_msg = SystemMessage(                  
+    '''Read the current knowledge base and expand it with the knowledge of the new paper. Give a full overview of all papers by consiering the current paper informaion and the base. Here is the base:''')
+
+paper_base_update_msg2 = SystemMessage(                  
+    '''And here is the new paper information:''')
+
+
+
 # Define AgentState as a TypedDict
 class AgentState(MessagesState):
     learning_preferences: Optional[str]
-    #paper_information: Optional[str]
     paper_title: Optional[str]
     paper_explanation: Optional[str]
-    #paper_base: Optional[str]
+    paper_base: Optional[str]
 
 def paper_assistant(state: AgentState) -> AgentState:
     result = llm_with_tools.invoke([sys_msg] + state["messages"])
-    print(f"LLM Result: {result}")
     return {"messages": [result]}
 
-
-    
+def paper_organizer(state: AgentState) -> AgentState:
+    paper_title = llm.invoke([paper_titel_msg] + state["messages"])
+    append_to_file("src/data/paper_list.txt", paper_title.content)
+    old_paper_base  =load_file("src/data/paper_base.txt", "No papers analyzed yet")# Read paper_base.txt
+    new_paper_base = llm.invoke([paper_base_update_msg] + old_paper_base + paper_base_update_msg2 + state["messages"])
+    save_file("src/data/paper_base.txt", new_paper_base.content)
+    return {"paper_title": paper_title.content, "paper_base": new_paper_base.content    }
 
 builder = StateGraph(AgentState)
 
 builder.add_node("paper_assistant", paper_assistant)
+builder.add_node("paper_organizer", paper_organizer)
 builder.add_node("tools", ToolNode(tools))
 
 builder.add_edge(START, "paper_assistant")
-builder.add_conditional_edges("paper_assistant", tools_condition)
+builder.add_conditional_edges("paper_assistant", tools_condition, {"tools": "tools", END: "paper_organizer"})
 builder.add_edge("tools", "paper_assistant")
+builder.add_edge("paper_organizer", END)
+
 
 graph = builder.compile()
-
-graph.invoke({
-    "messages": [HumanMessage("I want to learn about transformers")],
-    "learning_preferences": "transformers"
-})
 
 
 
